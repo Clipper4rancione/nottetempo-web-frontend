7ccaca988b59874997ed5a41334ce99ce114fe9e
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-05-30 20:33:18 +0200

    add jumbotron

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,17 +1,25 @@
 <script>
-    export default {
-        name: 'AppMain'
-    }
+export default {
+  name: "AppMain",
+};
 </script>
 
 <template>
-    <div>
-        M
-    </div>
+  <main>
+    <div id="jumbotron"></div>
+  </main>
 </template>
 
 
 
 <style lang="scss" scoped>
+#jumbotron {
+  height: 500px;
+  width: 100%;
+  background-image: url("../assets/img/jumbo_empty_yellow.png");
 
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+}
 </style>

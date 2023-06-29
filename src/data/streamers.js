b3112c955f2a86ed47bf3c213818cd5fc15441c4
Clipper4rancione <meluziis.ciro99@gmail.com>
2023-06-29 12:21:16 +0200

    add js file with streamers details

---
+++ src/data/streamers.js
@@ -0,0 +1,58 @@
+export default [
+    {
+        name: "ClipperArancione",
+        age: 23,
+        city: "Napoli",
+        bio: "Ciro aka Clipper, classe \"99, napoletano di sangue e di fatto, è il classico streamer che guardi quando il tuo amico inizia a parlare napoletano ma non lo fa bene e quindi hai bisogno di trovare qualcuno che lo sappia fare. Nelle sue streaming troverai tanto amore per il prossimo e la sacra arte del chilling supremo",
+        content: ["MOBA", "FPS", "Souls Like", "Just Chatting"],
+        instaLink: "",
+        ytLink: "",
+        twitchLink: "",
+        tiktokLink: "",
+    }
+    {
+        name: "Ikushimi",
+        age: 21,
+        city: "Genova",
+        bio: "",
+        content: [],
+        instaLink: "",
+        ytLink: "",
+        twitchLink: "",
+        tiktokLink: "",
+    },
+    {
+        name: "AlessioMolteni",
+        age: 29,
+        city: "Como",
+        bio: "",
+        content: [],
+        instaLink: "",
+        ytLink: "",
+        twitchLink: "",
+        tiktokLink: "",
+    },
+    {
+        name: "92Xander92",
+        age: 29,
+        city: "Messina",
+        bio: "",
+        content: [],
+        instaLink: "",
+        ytLink: "",
+        twitchLink: "",
+        tiktokLink: "",
+    },
+    {
+        name: "Kajyto1550",
+        age: 21,
+        city: "Genova",
+        bio: "",
+        content: [],
+        instaLink: "",
+        ytLink: "",
+        twitchLink: "",
+        tiktokLink: "",
+    }
+
+]
